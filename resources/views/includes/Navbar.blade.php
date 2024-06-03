c3e5e1888465b3f225bd71dcde55c1699c864242
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-06-03 16:57:00 +0700

    fix(views): fix navbar active base on routes

--- resources/views/includes/Navbar.blade.php
+++ resources/views/includes/Navbar.blade.php
@@ -28,102 +28,100 @@
             @guest <!-- Jika pengguna belum login -->
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+                    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page" href="/">Beranda</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/informasiTerkini">Informasi Terkini</a>
+                    <a class="nav-link {{ Request::is('informasiTerkini') ? 'active' : '' }}" href="/informasiTerkini">Informasi Terkini</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/kalkulatorGizi">Kalkulator</a>
+                    <a class="nav-link {{ Request::is('kalkulatorGizi') ? 'active' : '' }}" href="/kalkulatorGizi">Kalkulator</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/informasiAhliGizi">Ahli Gizi</a>
+                    <a class="nav-link {{ Request::is('informasiAhliGizi') ? 'active' : '' }}" href="/informasiAhliGizi">Ahli Gizi</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    @php
+                    $dropdownActive = Request::is('artikelPendidikanKesehatan') || Request::is('menuSehatBalita') ||
+                    Request::is('lokasiPelayananKesehatan') || Request::is('programKebijakan') ||
+                    Request::is('grafikGiziIndonesia') || Request::is('acaraKesehatanBalita');
+                    @endphp
+                    <a class="nav-link dropdown-toggle {{ $dropdownActive ? 'active' : '' }}" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Layanan Lainnya
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/artikelPendidikanKesehatan">Artikel Pendidikan Kesehatan</a></li>
+                        <li><a class="dropdown-item {{ Request::is('artikelPendidikanKesehatan') ? 'active' : '' }}" href="/artikelPendidikanKesehatan">Artikel Pendidikan Kesehatan</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li><a class="dropdown-item" href="/menuSehatBalita">Menu Sehat Balita</a></li>
+                        <li><a class="dropdown-item {{ Request::is('menuSehatBalita') ? 'active' : '' }}" href="/menuSehatBalita">Menu Sehat Balita</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li><a class="dropdown-item" href="/lokasiPelayananKesehatan">Lokasi Pelayanan Kesehatan</a></li>
+                        <li><a class="dropdown-item {{ Request::is('lokasiPelayananKesehatan') ? 'active' : '' }}" href="/lokasiPelayananKesehatan">Lokasi Pelayanan Kesehatan</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="/programKebijakan">Program & Kebijakan</a>
-                        </li>
+                        <li><a class="dropdown-item {{ Request::is('programKebijakan') ? 'active' : '' }}" href="/programKebijakan">Program & Kebijakan</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="/grafikGiziIndonesia">Grafik Gizi Indonesia</a>
-                        </li>
+                        <li><a class="dropdown-item {{ Request::is('grafikGiziIndonesia') ? 'active' : '' }}" href="/grafikGiziIndonesia">Grafik Gizi Indonesia</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="/acaraKesehatanBalita">Acara Kesehatan Balita</a>
-                        </li>
+                        <li><a class="dropdown-item {{ Request::is('acaraKesehatanBalita') ? 'active' : '' }}" href="/acaraKesehatanBalita">Acara Kesehatan Balita</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="btn bg-primary" href="/login">Masuk/Daftar</a>
+                    <a class="btn bg-primary {{ Request::is('login') ? 'active' : '' }}" href="/login">Masuk/Daftar</a>
                 </li>
             </ul>
             @else <!-- Jika pengguna sudah login -->
             @if (Auth::user()->role == 'user')
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/beranda/auth">Beranda</a>
+                    <a class="nav-link {{ Request::is('beranda/auth') ? 'active' : '' }}" aria-current="page" href="/beranda/auth">Beranda</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/informasiTerkini/auth">Informasi Terkini</a>
+                    <a class="nav-link {{ Request::is('informasiTerkini/auth') ? 'active' : '' }}" href="/informasiTerkini/auth">Informasi Terkini</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/kalkulatorGizi/auth">Kalkulator</a>
+                    <a class="nav-link {{ Request::is('kalkulatorGizi/auth') ? 'active' : '' }}" href="/kalkulatorGizi/auth">Kalkulator</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/informasiAhliGizi/auth">Ahli Gizi</a>
+                    <a class="nav-link {{ Request::is('informasiAhliGizi/auth') ? 'active' : '' }}" href="/informasiAhliGizi/auth">Ahli Gizi</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    @php
+                    $dropdownActive = Request::is('artikelPendidikanKesehatan/auth') || Request::is('menuSehatBalita/auth') ||
+                    Request::is('lokasiPelayananKesehatan/auth') || Request::is('programKebijakan/auth') ||
+                    Request::is('grafikGiziIndonesia/auth') || Request::is('acaraKesehatanBalita/auth');
+                    @endphp
+                    <a class="nav-link dropdown-toggle {{ $dropdownActive ? 'active' : '' }}" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Layanan Lainnya
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/artikelPendidikanKesehatan/auth">Artikel Pendidikan Kesehatan</a></li>
+                        <li><a class="dropdown-item {{ Request::is('artikelPendidikanKesehatan/auth') ? 'active' : '' }}" href="/artikelPendidikanKesehatan/auth">Artikel Pendidikan Kesehatan</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li><a class="dropdown-item" href="/menuSehatBalita/auth">Menu Sehat Balita</a></li>
+                        <li><a class="dropdown-item {{ Request::is('menuSehatBalita/auth') ? 'active' : '' }}" href="/menuSehatBalita/auth">Menu Sehat Balita</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li><a class="dropdown-item" href="/lokasiPelayananKesehatan/auth">Lokasi Pelayanan Kesehatan</a></li>
+                        <li><a class="dropdown-item {{ Request::is('lokasiPelayananKesehatan/auth') ? 'active' : '' }}" href="/lokasiPelayananKesehatan/auth">Lokasi Pelayanan Kesehatan</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="/programKebijakan/auth">Program & Kebijakan</a>
-                        </li>
+                        <li><a class="dropdown-item {{ Request::is('programKebijakan/auth') ? 'active' : '' }}" href="/programKebijakan/auth">Program & Kebijakan</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="/grafikGiziIndonesia/auth">Grafik Gizi Indonesia</a>
-                        </li>
+                        <li><a class="dropdown-item {{ Request::is('grafikGiziIndonesia/auth') ? 'active' : '' }}" href="/grafikGiziIndonesia/auth">Grafik Gizi Indonesia</a></li>
                         <li>
                             <hr class="dropdown-divider" />
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="/acaraKesehatanBalita/auth">Acara Kesehatan Balita</a>
-                        </li>
+                        <li><a class="dropdown-item {{ Request::is('acaraKesehatanBalita/auth') ? 'active' : '' }}" href="/acaraKesehatanBalita/auth">Acara Kesehatan Balita</a></li>
                     </ul>
                 </li>
                 <!-- Profile Name -->
@@ -139,7 +137,9 @@
                 <!-- Logout Button -->
                 <li class="nav-item">
                     <form action="" style="width: 40px; height: 40px;">
-                        <button type="button" class="btn btn-primary rounded-circle text-white w-100 h-100" data-bs-toggle="modal" data-bs-target="#confirmationLogout"><i class="fa-solid fa-person-walking-dashed-line-arrow-right"></i></button>
+                        <button type="button" class="btn btn-primary rounded-circle text-white w-100 h-100" data-bs-toggle="modal" data-bs-target="#confirmationLogout">
+                            <i class="fa-solid fa-person-walking-dashed-line-arrow-right"></i>
+                        </button>
                     </form>
                 </li>
             </ul>
