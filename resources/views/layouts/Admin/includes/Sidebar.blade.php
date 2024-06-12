8395f46e5fbe56b018b4b3f22c12e35d8024552a
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-06-12 20:12:43 +0700

    feat(views, asset): add new sidebar views and css for admin

--- resources/views/layouts/Admin/includes/Sidebar.blade.php
+++ resources/views/layouts/Admin/includes/Sidebar.blade.php
@@ -1,13 +1,22 @@
-<div class="bg-primary-subtle border-end border-dark" id="sidebar-wrapper">
-    <div class="sidebar-heading text-center py-4 text-primary fs-4 fw-bold text-uppercase"><i class="logo"></i>PELITA</div>
+<div class="bg-sidebar" id="sidebar-wrapper">
+    <div class="sidebar-heading text-center py-4 text-primary fs-4 fw-bold text-uppercase mt-3">
+        <img src="{{ asset('Main/assets/main/Logo-secondary.svg') }}" alt="">
+    </div>
     <div class="list-group list-group-flush my-3">
-        <a href="/adminView" class="list-group-item list-group-item-action bg-primary-subtle active border-bottom border-dark"><i class="fas fa-tachometer-alt me-2 text-dark"></i>Dashboard</a>
-        <a href="/adminDokterRequest" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-project-diagram me-2 text-dark"></i>Dokter Request</a>
-        <!-- <a href="#" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-chart-line me-2 text-dark"></i>Informasi Dokter</a> -->
-        <!-- <a href="/adminLokasiPelayananKesehatan" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-shopping-cart me-2 text-dark"></i>Lokasi Pelayanan Kesehatan</a> -->
-        <a href="/adminProgramKebijakan" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-gift me-2 text-dark"></i>Program & Kebijakan</a>
-        <!-- <a href="#" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-comment-dots me-2 text-dark"></i>Grafik Gizi Indonesia</a> -->
-        <a href="/adminAcaraKesehatanBalita" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-chart-line me-2 text-dark"></i>Acara Kesehatan</a>
-        <a href="/adminArtikelPendidikanKesehatan" class="list-group-item list-group-item-action bg-primary-subtle border-bottom border-dark"><i class="fas fa-map-marker-alt me-2 text-dark"></i>Artikel Pendidikan Kesehatan</a>
+        <a href="/adminView" class="sidebar-link w-100 ms-3 mb-2 {{ Request::is('adminView') ? 'active' : '' }}">
+            <i class="fa-solid fa-gauge-high fs-4 me-3"></i>Dashboard
+        </a>
+        <a href="/adminDokterRequest" class="sidebar-link w-100 ms-3 mb-2 {{ Request::is('adminDokterRequest') ? 'active' : '' }}">
+            <i class="fa-solid fa-user-doctor fs-4 me-3"></i>Dokter Request
+        </a>
+        <a href="/adminProgramKebijakan" class="sidebar-link w-100 ms-3 mb-2 {{ Request::is('adminProgramKebijakan') ? 'active' : '' }}">
+            <i class="fa-solid fa-users-between-lines fs-4 me-2"></i>Program & Kebijakan
+        </a>
+        <a href="/adminAcaraKesehatanBalita" class="sidebar-link w-100 ms-3 mb-3 {{ Request::is('adminAcaraKesehatanBalita') ? 'active' : '' }}">
+            <i class="fa-solid fa-house-laptop fs-4 me-2"></i>Acara Kesehatan
+        </a>
+        <a href="/adminArtikelPendidikanKesehatan" class="sidebar-link w-100 ms-3 {{ Request::is('adminArtikelPendidikanKesehatan') ? 'active' : '' }}">
+            <i class="fa-solid fa-book-medical fs-4 me-3"></i>Artikel Pendidikan Kesehatan
+        </a>
     </div>
 </div>
